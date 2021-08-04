b59c0b776c2616ac9a42ec25596438dd3affeec2
neavemj <matthewjneave1@gmail.com>
2021-08-04 17:21:20 +1000

    minor modification

--- scripts/draw_ggtree.R
+++ scripts/draw_ggtree.R
@@ -1,12 +1,14 @@
 # script to draw ggtree in the phylogenetics module
 # Matthew Neave 2021-08-04
 
+# required libraries - need to add to conda environment
 library(ggtree)
 library(ape)
 library(ggplot2)
 library(phangorn)
 
-# the below file is specified as an input in the snakemake rule
+# the below files are specified as inputs in the snakemake rule
+# if we do it this way, need to ensure filenames exactly match
 
 tree = read.tree("04_phylogenetics/HA-sequences.treefile")
 metadata = read.table("04_phylogenetics/HA-sequences.metadata.txt")
@@ -18,6 +20,7 @@ colnames(metadata) <- c("Sample", "Source")
 mid_root <- midpoint(tree)
 
 # add color for source
+# this information comes from the metadata rule
 
 cols <- c(
   "NEW" = "#2ecc71",
@@ -33,4 +36,5 @@ p <- ggtree(mid_root) %<+% metadata +
   scale_fill_manual(values = cols, na.value="grey30") +
   xlim(values=c(0, 1.0))
 
+# use ggsave for the plot - required as an output in snakemake
 ggsave("04_phylogenetics/HA-sequences.tree.pdf", width=10, height=10, limits=F)
